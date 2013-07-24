8ba76cf76e40f8bc34cd6a2ccdbe14670c043254
diasansley <ansley@ajency.in>
2013-07-24 11:31:35 +0530

    config file set(local & server),refresh token fix,update query fix

--- config.php
+++ config.php
@@ -1,18 +1,30 @@
 <?php
+
 /*
-------------------------------------------------------
+  ------------------------------------------------------
   www.idiotminds.com
---------------------------------------------------------
-*/
+  --------------------------------------------------------
+ */
 session_start();
 
-$base_url= filter_var('http://localhost/fatboyslim/', FILTER_SANITIZE_URL);
+if ($_SERVER['HTTP_HOST'] == "localhost") {
+    $base_url = filter_var('http://localhost/fatboyslim/', FILTER_SANITIZE_URL);
+    define('CLIENT_ID', '1082310462133.apps.googleusercontent.com');
+    define('CLIENT_SECRET', 'fSQZVeztcNKPnx2qYrOxSQv3');
+    define('REDIRECT_URI', 'http://localhost/fatboyslim/dashboard.php');
+    define('APPROVAL_PROMPT', 'force');
+    define('ACCESS_TYPE', 'offline');
+} else {
+    $base_url = filter_var('http://ajency.in/hospice/', FILTER_SANITIZE_URL);
 
 // Visit https://code.google.com/apis/console to generate your
 // oauth2_client_id, oauth2_client_secret, and to register your oauth2_redirect_uri.
 
-define('CLIENT_ID','1082310462133.apps.googleusercontent.com');
-define('CLIENT_SECRET','fSQZVeztcNKPnx2qYrOxSQv3');
-define('REDIRECT_URI','http://localhost/fatboyslim/dashboard.php');
-define('APPROVAL_PROMPT','auto');
-define('ACCESS_TYPE','offline');
+    define('CLIENT_ID', '562168324591.apps.googleusercontent.com');
+    define('CLIENT_SECRET', '82dI4px-fB0kufBQQ6wyZbND');
+    define('REDIRECT_URI', 'http://ajency.in/hospice/dashboard.php');
+    define('APPROVAL_PROMPT', 'force');
+    define('ACCESS_TYPE', 'offline');
+}
+
+
